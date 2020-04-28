845b78bfbd482946bb33ca98b6b64c5e8ca17153
Yoren Schriever <yoren@ocelot.nl>
2020-04-28 14:43:30 +0200

    apc mini buttons

--- src/lib/apcmini/apcmini.cpp
+++ src/lib/apcmini/apcmini.cpp
@@ -1,6 +1,9 @@
 #include "apcmini.h"
 #include "../midi/midi.h"
 
+bool APCMini::allFlash=false;
+bool APCMini::allStatus[width*height];
+
 void APCMini::Initialize()
 {
     Midi::Initialize();
@@ -11,16 +14,85 @@ void APCMini::Initialize()
 
 void APCMini::handleNoteOff(uint8_t channel, uint8_t note, uint8_t velocity)
 {
-    //handle as a noteon with velocity 0
-    handleNoteOn(channel, note, 0);
+    if (channel != midichannel)
+        return;
+    handleKeyPress(note,false);
 }
 
 void APCMini::handleNoteOn(uint8_t channel, uint8_t note, uint8_t velocity)
 {
-    Midi::sendNoteOn(channel,note,velocity > 0 ? 100:0);
-    Midi::waitTxDone();
+    if (channel != midichannel)
+        return;
+    handleKeyPress(note,velocity>0);
 }
 
 void APCMini::handleControllerChange(uint8_t channel, uint8_t note, uint8_t velocity){
+    if (channel != midichannel)
+        return;
+}
 
+
+void APCMini::handleKeyPress(uint8_t note, bool ison)
+{
+
+    if (note == 82)
+    {
+        if (ison)
+            toggleFlashAll();
+        return;
+    }
+
+    int flashBorder = allFlash ? (height * width) : (flashrows * width);
+    if (note >= 0 && note < flashBorder){
+        setNote(note,ison, false);
+    } else if (ison && note <= (height+1)*width) { 
+        //a button was pressed, not released 
+        if (note >= height*width) 
+        {
+            //stop buttons
+            releaseGroup(note,false);        
+        } 
+        else 
+        {
+            //other patterns
+            boolean newstat = !allStatus[note];
+            releaseGroup(note,false);
+            setNote(note, newstat, false);
+        }
+    }
+}
+
+void APCMini::toggleFlashAll()
+{
+    allFlash = !allFlash;
+    Midi::sendNoteOn(midichannel, 82,allFlash*100);
+    Midi::waitTxDone(); 
+    setAllOff();
+}
+
+void APCMini::setAllOff()
+{
+    for (byte note=0; note<height*width; note++)
+        setNote(note,false,true);
+}
+
+void APCMini::setNote(uint8_t note, bool ison, bool force)
+{
+    if (allStatus[note] == ison && !force)
+        return;
+
+    allStatus[note] = ison;
+
+    //set led on APC
+    Midi::sendNoteOn(midichannel, note,ison*100);
+    Midi::waitTxDone(); 
+    //MIDI.sendNoteOn(note, ison*100, 1); 
+    //delay(2); //must be 2, 1 doesnt work
+}
+
+void APCMini::releaseGroup(uint8_t note, bool recursiveCall)
+{
+    byte bottom = note%width;
+    for (byte i=bottom; i < height*width; i+=width)
+        setNote(i,false, false);
 }
--- src/lib/apcmini/apcmini.h
+++ src/lib/apcmini/apcmini.h
@@ -2,6 +2,11 @@
 
 #include <inttypes.h>
 
+const int flashrows = 3;
+const int width=8;
+const int height=8;
+const int midichannel = 0; //0 = channel 1
+
 class APCMini {
 
     public:
@@ -12,5 +17,14 @@ class APCMini {
         static void handleNoteOn(uint8_t channel, uint8_t note, uint8_t velocity);
         static void handleNoteOff(uint8_t channel, uint8_t note, uint8_t velocity);
         static void handleControllerChange(uint8_t channel, uint8_t note, uint8_t velocity);
+
+        static bool allFlash;
+        static bool allStatus[width*height];
+
+        static void handleKeyPress(uint8_t note, bool ison);
+        static void toggleFlashAll();
+        static void setAllOff();
+        static void setNote(uint8_t note, bool ison, bool force);
+        static void releaseGroup(uint8_t note, bool recursiveCall);
 
 };
