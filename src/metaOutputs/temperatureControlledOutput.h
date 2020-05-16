dda8108995534bdd20cf7707afc09c01956caee3
Yoren Schriever <yoren@ocelot.nl>
2020-05-16 22:11:32 +0200

    tempraturecontrol progression

--- src/metaOutputs/temperatureControlledOutput.h
+++ src/metaOutputs/temperatureControlledOutput.h
@@ -22,29 +22,43 @@ public:
         float equilibriumduty = cooldownCoefficient * (maxTemperature - ambientTemperature) / heatupCoefficient;
         float equilibriumdutycomplement = 1.0 - equilibriumduty;
 
+        parameters.heatupCoefficient = heatupCoefficient;       //how fast the lamp will heat up if it is fully on
+        parameters.cooldownCoefficient = cooldownCoefficient;   //how fast the lamp will cool down if it is off
+        parameters.maxTemperature = maxTemperature;             //the maximum temperature we allow
+        parameters.ambientTemperature = ambientTemperature;     //the temperature of the room
+        parameters.maxtemperatureDescent = 0.999;               //this makes sure the doesnt cool down too fast, if the model work work corrently we wouldnt need this
+        parameters.equilibriumDutyCycleComplement = equilibriumdutycomplement;  //1-the equilibriumdutycycle. The equilibrium duty cycle is pwm duty cycle at which the heatup and cooldown are equal, and the lamp would exentuallt exactly reach the maximum temperature.
+        parameters.correctionThresholdTemperature = ambientTemperature + (maxTemperature-ambientTemperature)*0.75; //The temperature where the model start scaling down. If the temperature is below this value the requested duty cycle doesn't have to be corrected.
+
         SetLength(1);
     }
 
     //index and size are in bytes
     void SetData(uint8_t *data, int size, int index)
     {
+        //store the requested colour, so we can use it to further dim the values
+        //if no input comes in, but the lamp is so bright it will overheat.
+        memcpy(this->pixelData + index, data, copylength);
+
         if (size != sizeof(T_COLOUR))
             return; //currently only works if setdata is called pixel by pixels
 
-        //Todo: also scale existing data if no new input comes in
-        //create a task for this?
-        T_COLOUR colour = *(data);
-
+        //calculate the time between the last update and the new update.
         float dt = 0.1; //TODO
 
-        //make it monochromatic, in case it wasn't already
+        //the model needs the requested intensity as a value between 0 and 1.
+        //to do this, first read the colour from the data argument, 
+        //then make it monochromatic, in case it wasn't already, get the
+        //brightness value and device by 255.
+        T_COLOUR colour = *(data);
         Monochrome monochrome = static_cast<Monochrome>(colour);
-
         float requestedIntensity = float(monochrome.L) / 255;
+
+        //let the model calculate the correction value
         float correctedIntensity = models[i].getCorrectedIntensity(requestedIntensity, dt);
 
+        //dim the colour and send it to the child Output
         colour.dim(correctedIntensity * 255);
-
         childOutput->SetData(&colour, sizeof(T_COLOUR), index);
     }
 
@@ -60,16 +74,20 @@ public:
 
     void ShowGroup()
     {
-        //showgroup will be called regularly,even if there is no new input.
-        //I use this to perform additional temperature correction
+        //showgroup will be called regularly, even if there is no new input.
+        //I use this to perform additional temperature correction in case no
+        //new data comes in, and setdata therefore cannot perform the correction.
+        float dt = todo
+        
         if (dt > 100)
         {
-            for
-                *(int i = 0; i < length; i++)
-                {
-                    //todo remember the orinal colour so we can
-                    SetData()
-                }
+            for (int i = 0; i < length; i++)
+            {
+                float correctedIntensity = models[i].getCorrectedIntensity(model[i].previousRequestedIntensity, dt);
+                T_COLOUR dimmed = pixelData[i];
+                dimmed.dim(correctedIntensity * 255);
+                childOutput->SetData(&dimmed, sizeof(T_COLOUR), i);
+            }
         }
         childOutput->ShowGroup();
     }
@@ -88,13 +106,17 @@ public:
     {
         this->length = len / sizeof(T_COLOUR);
 
-        temp
+        pixelData = realloc(pixelData,len);
+        models = realloc(models,length * sizeof(TemperatureModel));
+        for (inti=0;i<length;i++)
+            models[i] = TemperatureModel(&parameters,parameters->maxTemperature)
     }
 
 private:
     Output *childOutput;
     int length;
-    TemperatureModel *models;
+    TemperatureModel *models = NULL;
+    T_COLOUR *pixelData = NULL;
 
     TemperatureModelParameters parameters;
 };
@@ -106,6 +128,8 @@ struct TemperatureModelParameters
     float maxTemperature;
     float ambientTemperature;
     float maxtemperatureDescent; // = 0.999
+    float equilibriumDutyCycleComplement;
+    float correctionThresholdTemperature;
 };
 
 class TemperatureModel
@@ -161,14 +185,14 @@ private:
         if (temp < oldtemp)
             slowtemp *= Parameters->maxtemperatureDescent;
 
-        //if the actual temp becomes higer that out slow temp, that is more important to avoid overheating, so
+        //if the actual temp becomes higer than the slow temp, that is more important to avoid overheating, so
         //pickup that value immideately
         return fmax(slowtemp, temp);
     }
 
     float calcPwmScale(float temp)
     {
-        float temprange = fminf(fmaxf((temp - tempthresh) / (tempmax - tempthresh), 0), 1);
-        return 1.0 - equilibriumdutycomplement * temprange;
+        float temprange = fminf(fmaxf((temp - Parameters->correctionThresholdTemperature) / (Parameters->maxTemperature - Parameters->correctionThresholdTemperature), 0), 1);
+        return 1.0 - Parameters->equilibriumDutyCycleComplement * temprange;
     }
 }
