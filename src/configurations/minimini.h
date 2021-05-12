b6f5bc148435a941ce03349dbe63f4e2b64d19e5
Yoren Schriever <yoren@ocelot.nl>
2021-05-12 16:12:27 +0200

    pwm on minimini

--- src/configurations/minimini.h
+++ src/configurations/minimini.h
@@ -19,14 +19,46 @@
 #include "inputs/layeredApcminiInput.h"
 #include "inputs/switchableInput.h"
 
-LUT *SunstripLut = new GammaLUT(2.5, 255);
+LUT *SunstripLut = new GammaLUT(2.6, 255);
+LUT *GammaLut12 = new GammaLUT(2.5, 4096);
 
 void LoadConfiguration()
 {
 
     Configuration.hostname = "minimini";
     
+    Configuration.tapMidiNote = 98;
+    Configuration.tapStopMidiNote = 89;
+    Configuration.tapAlignMidiNote = 88;
+    Configuration.tapBarAlignMidiNote = 87;
+
     Configuration.pipes = {
+
+        //FAIRY
+        new Pipe(
+            new SwitchableInput(
+                new LayeredApcminiInput<Monochrome>(
+                1,
+                1,
+                new LayeredPattern<Monochrome> *[8] {
+                    new Layered::BlinderPattern(Transition::none, Transition::none,255,100,1500),
+                    new Layered::SinPattern(4,3.1415),
+                    new Layered::BeatAllFadePattern(1000),
+                    new Layered::BeatAllFadePattern(150),
+                    new Layered::SlowStrobePattern(),
+
+                    new Layered::BlinderPattern(Transition::none, Transition::none,255,100,1500),
+                    new Layered::SlowStrobePattern(),
+                    new Layered::FastStrobePattern(),
+                }),
+                new LayeredPatternInput<Monochrome>(1,new Layered::BlinderPattern()),
+                Midi::isConnected
+            ),
+            new PWMOutput(1),
+            Pipe::transfer<Monochrome, Monochrome12>,
+            GammaLut12),
+
+
         // new Pipe(
         //     // new CombinedInput({
         //     //     {input: new PatternInput<Monochrome>(5, new SinPattern()), offset:0},
@@ -45,7 +77,7 @@ void LoadConfiguration()
                 3,
                 new LayeredPattern<Monochrome> *[8] {
                     new Layered::BlinderPattern(Transition::fromCenter, Transition::fromSides,150,1000,1500),
-                    new Layered::GlowPattern(),
+                    new Layered::GlowPattern(3),
                     new Layered::SinPattern(20),
                     new Layered::ChaseFromCenterPattern(1200),
                     new Layered::BeatMultiFadePattern(500),
@@ -54,7 +86,7 @@ void LoadConfiguration()
                     new Layered::GlitchPattern(),
                     new Layered::BeatAllFadePattern(600),
                 }),
-                new LayeredPatternInput<Monochrome>(10,new Layered::GlowPattern()),
+                new LayeredPatternInput<Monochrome>(34,new Layered::SinPattern(20)),
                 Midi::isConnected
             ),
             new NeopixelOutput<Kpbs800>(7),
