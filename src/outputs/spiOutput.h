951bf77383ba5874103b3171a1d99c61a0301a69
Yoren Schriever <yoren@ocelot.nl>
2022-08-19 08:19:29 +0200

    spi support for multiple devices

--- src/outputs/spiOutput.h
+++ src/outputs/spiOutput.h
@@ -5,13 +5,14 @@
 #include "hardware/spi/spi.h"
 #include "debug.h"
 
-//SpiOutput sends the output over spi.
-//Can be used for pixel led strands.
+// SpiOutput sends the output over spi.
+// Can be used for pixel led strands.
 class SpiOutput : public Output
 {
 public:
     SpiOutput(uint8_t clkPin, uint8_t dataPin, int frq = 1000000)
     {
+        spi = SPI::CreateInstance();
         this->clkPin = clkPin;
         this->dataPin = dataPin;
         this->frq = frq;
@@ -20,7 +21,7 @@ public:
         buffer = (uint8_t *)heap_caps_malloc(12, MALLOC_CAP_DMA | MALLOC_CAP_32BIT);
     }
 
-    //index and size are in bytes
+    // index and size are in bytes
     void SetData(uint8_t *data, int size, int index)
     {
         int copylength = min(size, length - index);
@@ -30,21 +31,21 @@ public:
 
     boolean Ready()
     {
-        //even though the datasheet states less, 1000us seems to be neccesary for 
-        //stable operation (tested on ledster)
-        return SPI::IsReadyForUs(1000); 
+        // even though the datasheet states less, 1000us seems to be necessary for
+        // stable operation (tested on ledster)
+        return spi->IsReadyForUs(1000);
     }
 
     void Show()
     {
-        SPI::Send(buffer, length);
+        spi->Send(buffer, length);
     }
 
     void ShowGroup() {}
 
     void Begin() override
     {
-        SPI::Initialize(pins[dataPin], pins[clkPin], frq);
+        spi->Initialize(pins[dataPin], pins[clkPin], frq);
     }
 
     void Clear()
@@ -52,22 +53,23 @@ public:
         memset(buffer, 0, length);
     }
 
-    //length is in bytes
+    // length is in bytes
     void SetLength(int len) override
     {
         if (len != this->length)
         {
-            //wait for the buffer to be sent before we are going to change its size
+            // wait for the buffer to be sent before we are going to change its size
             while (!Ready())
                 delay(1);
 
-            buffer = (uint8_t *)heap_caps_realloc(buffer, len , MALLOC_CAP_DMA | MALLOC_CAP_32BIT);
+            buffer = (uint8_t *)heap_caps_realloc(buffer, len, MALLOC_CAP_DMA | MALLOC_CAP_32BIT);
 
             this->length = len;
         }
     }
 
 private:
+    SPI *spi;
     uint8_t clkPin;
     uint8_t dataPin;
     int frq;
