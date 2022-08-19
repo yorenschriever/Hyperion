eee1c3d5f382a619a4ad9b4fd3df19ef7145741b
Yoren Schriever <yoren@ocelot.nl>
2022-08-19 09:05:39 +0200

    better usb debug

--- debug_usb.sh
+++ debug_usb.sh
@@ -1,2 +1,4 @@
 # to quit, press ctrl + A + K, then y
-screen $(ls /dev/cu.usbserial*) 115200,onlcr
+#screen $(ls /dev/cu.usbserial*) 115200,onlcr
+
+(sleep 0.5 && stty -f $(ls /dev/cu.usbserial*) 115200) & cat $(ls /dev/cu.usbserial*)
