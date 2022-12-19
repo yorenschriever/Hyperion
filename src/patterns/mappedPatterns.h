b6c8bdd2a236bf048e17ebd812fa61ef111e8029
Yoren Schriever <yoren@ocelot.nl>
2022-12-19 17:20:30 +0100

    rotaryPatternInput

--- src/patterns/mappedPatterns.h
+++ src/patterns/mappedPatterns.h
@@ -2,8 +2,9 @@
 #include "pattern.h"
 #include "helpers/pixelMap.h"
 #include "helpers/fade.h"
+#include "helpers/random.h"
 #include <vector>
-#include <math.h> 
+#include <math.h>
 
 namespace Mapped
 {
@@ -87,8 +88,7 @@ namespace Mapped
             Fade<Down, Linear>(200, WaitAtEnd),
             Fade<Down, Linear>(200, WaitAtEnd),
             Fade<Down, Linear>(200, WaitAtEnd),
-            Fade<Down, Linear>(200, WaitAtEnd)
-        };
+            Fade<Down, Linear>(200, WaitAtEnd)};
         int triggerNext = 0;
         bool lastActive = false;
 
@@ -111,12 +111,13 @@ namespace Mapped
             {
                 float distance = std::abs(map[index].x) + std::abs(map[index].y);
                 int delay = distance * 200;
-                float l = fades[0].getValue(delay) + 
-                    fades[1].getValue(delay) + 
-                    fades[2].getValue(delay) + 
-                    fades[3].getValue(delay) + 
-                    fades[4].getValue(delay);
-                if (l>1) l=1;
+                float l = fades[0].getValue(delay) +
+                          fades[1].getValue(delay) +
+                          fades[2].getValue(delay) +
+                          fades[3].getValue(delay) +
+                          fades[4].getValue(delay);
+                if (l > 1)
+                    l = 1;
 
                 pixels[index] += Params::getHighlightColour() * l;
             }
@@ -138,7 +139,8 @@ namespace Mapped
             this->map = map;
             this->lfo = LFO<SawDown>(period);
             this->lfo.setSkew(0.25);
-            std::transform(map.begin(), map.end(), std::back_inserter(scaledAngles), [](PixelPosition pos) -> float { return (atan2(pos.y,pos.x)+PI) / (2*PI); });
+            std::transform(map.begin(), map.end(), std::back_inserter(scaledAngles), [](PixelPosition pos) -> float
+                           { return (atan2(pos.y, pos.x) + PI) / (2 * PI); });
         }
 
         inline void Calculate(RGBA *pixels, int width, bool active) override
@@ -148,9 +150,70 @@ namespace Mapped
 
             for (int index = 0; index < std::min(width, (int)map.size()); index++)
             {
-                RGBA colour = Params::getSecondaryColour(); 
+                RGBA colour = Params::getSecondaryColour();
                 float lfoVal = lfo.getValue(scaledAngles[index]);
                 RGBA dimmedColour = colour * transition.getValue() * lfoVal;
+                pixels[index] += dimmedColour;
+            }
+        }
+    };
+
+    class HorizontalGradientPattern : public LayeredPattern<RGBA>
+    {
+        PixelMap map;
+        Transition transition = Transition(
+            200, Transition::none, 0,
+            1000, Transition::none, 0);
+
+    public:
+        HorizontalGradientPattern(PixelMap map)
+        {
+            this->map = map;
+        }
+
+        inline void Calculate(RGBA *pixels, int width, bool active) override
+        {
+            if (!transition.Calculate(active))
+                return;
+
+            for (int index = 0; index < std::min(width, (int)map.size()); index++)
+            {
+                float phase = (map[index].x + 1) / 2.;
+                RGBA colour = Params::getSecondaryColour() + Params::getPrimaryColour() * phase;
+                RGBA dimmedColour = colour * transition.getValue();
+                pixels[index] += dimmedColour;
+            }
+        }
+    };
+
+    class HorizontalDitheredGradientPattern : public LayeredPattern<RGBA>
+    {
+        PixelMap map;
+        Transition transition = Transition(
+            200, Transition::none, 0,
+            1000, Transition::none, 0);
+        std::vector<float> noise;
+        const size_t maxNoiseMapSize = 100;
+
+    public:
+        HorizontalDitheredGradientPattern(PixelMap map)
+        {
+            this->map = map;
+            for (int i = 0; i < std::min(map.size(), maxNoiseMapSize); i++)
+            {
+                noise.push_back((stableRandomFloat() - 0.5) * 1.3);
+            }
+        }
+
+        inline void Calculate(RGBA *pixels, int width, bool active) override
+        {
+            if (!transition.Calculate(active))
+                return;
+
+            for (int index = 0; index < std::min(width, (int)map.size()); index++)
+            {
+                RGBA colour = (map[index].x + noise[index % maxNoiseMapSize] < 0) ? Params::getSecondaryColour() : Params::getPrimaryColour();
+                RGBA dimmedColour = colour * transition.getValue();
                 pixels[index] += dimmedColour;
             }
         }
